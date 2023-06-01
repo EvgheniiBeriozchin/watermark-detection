78a7cee73315508d10a2f66c29284995ebcf04f6
EvgheniiBeriozchin <evghenii@r109017.ga1.swh.mhn.de>
2023-06-02 00:15:40 +0200

    Update comparison script with basic comparison function

--- compare_drawings.py
+++ compare_drawings.py
@@ -3,15 +3,20 @@ import cv2
 
 from argparse import ArgumentParser
 from numpy import ndarray
-from typing import List
+from typing import List, Tuple
 from matplotlib import pyplot as plt
 
 IMAGE_DATABASE_PATH = "../../data/image-database/"
 
 def compare_image_pair(image1: ndarray, image2: ndarray):
-    return 0.0
+    contours1, _ = cv2.findContours(image1, 2, 1)
+    contours2, _ = cv2.findContours(image2, 2, 1)
 
-def get_similar_images(target_image: str, *, n_images: int):
+    match = cv2.matchShapes(contours1[1], contours2[1], 3, 0.0)
+    
+    return -match
+
+def get_similar_images(target_image: ndarray, *, n_images: int):
     most_similar_images = []
     
     for reference_image_path in os.listdir(IMAGE_DATABASE_PATH):
@@ -27,19 +32,19 @@ def get_similar_images(target_image: str, *, n_images: int):
         
     return most_similar_images
             
-def display_similar_images(target_image: ndarray, similar_images: List[ndarray]):
-    _, axes = plt.subplots(len(similar_images), 2, figsize=(10, 4 * len(similar_images)))
+def display_similar_images(target_image: ndarray, similar_images: List[Tuple[ndarray, float]]):
+    figure, axes = plt.subplots(len(similar_images), 2, figsize=(10, 4 * len(similar_images)))
     
     for index, reference_image in enumerate(similar_images):
         axes[index, 0].imshow(target_image)
         axes[index, 1].imshow(reference_image)
-    
 
 if __name__=='__main__':
     argument_parser = ArgumentParser()
     argument_parser.add_argument("-i", "--image", help="image to be compared")
     arguments = argument_parser.parse_args()
     
-    target_image = cv2.imread(arguments.image)
+    target_image = cv2.imread(arguments.image, cv2.IMREAD_GRAYSCALE)
     similar_images = get_similar_images(target_image, n_images=5)
-    display_similar_images(target_image, [image_pair[0] for image_pair in similar_images])
+    print('Similarities: {}'.format([similarity for _, similarity in similar_images]))
+    display_similar_images(target_image, [image for image, _ in similar_images])
