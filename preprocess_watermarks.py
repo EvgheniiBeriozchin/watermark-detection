b77ad09a8fd975e91f5403d2b3efed211083fd2d
Paulina <paulina.weyh@tum.de>
2023-06-15 09:47:52 +0200

    Preprocessing Script for cropped watermarks

--- preprocess_watermarks.py
+++ preprocess_watermarks.py
@@ -2,8 +2,7 @@ from numpy import ndarray
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
-import glob
-import sys
+
 
 
 def strech_contrast(image):
@@ -17,7 +16,7 @@ def strech_contrast(image):
 
 
 def remove_ink(image):
-
+    """Remove ink and dark colors on images"""
     if len(image.shape) < 3:
         image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
     s = 100
@@ -40,7 +39,7 @@ def remove_darker_foreground(image, structuring_element_size = 6, iterations = 7
 
 
 def top_minus_back(image):
-    # Top Hat
+    """Remove noisy background from image"""
     k1 = 16
     filterSize = (k1, k1)
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, filterSize)
@@ -48,16 +47,11 @@ def top_minus_back(image):
     if len(image.shape) > 2:
         input_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # Applying the Top-Hat operation
     tophat_img = cv2.morphologyEx(input_image, cv2.MORPH_TOPHAT, kernel)
-
-    # Black-Hat
     k2 = 6
     filterSize2 = (k2, k2)
     kernel2 = cv2.getStructuringElement(cv2.MORPH_RECT, filterSize2)
-    # Applying the Black-Hat operation
     blackhat_img = cv2.morphologyEx(input_image, cv2.MORPH_BLACKHAT, kernel2)
-
     filtered = cv2.absdiff(tophat_img, blackhat_img)
     return filtered
 
@@ -81,35 +75,15 @@ def im_show(image, name=None):
 
 def preprocess_watermark(image: ndarray):
     image = strech_contrast(image)
-    #im_show(image, name="Streched")
     image = remove_ink(image)
-    #im_show(image, name="remove_ink")
     image = top_minus_back(image)
     image_store = image
-    #im_show(image, name="top_black")
     image, eroded, dilated = remove_darker_foreground(image)
-    #im_show(image, name="remove_dark")
     image = remove_nonuniform_background(image)
-    #im_show(image, name="remove_nonuni")
     image = cv2.GaussianBlur(image, (3, 3), 0)
-    #im_show(image, name="Blur")
     alpha = 0.3
     image = cv2.addWeighted(image, alpha, image_store, 1-alpha, 0.0)
-    #im_show(image, name="weighted")
 
     return image
 
 
-## Test
-def load_comparison_watermarks(folder_path):
-    """Read comparison images and transform to binarized negative of sketch"""
-    images = [cv2.imread(file) for file in glob.glob(folder_path + '/*.png')]
-    #images = [cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) for img in images]
-    return images
-
-path = "/Users/pauli/Documents/Studium/Master/4_Semester/TUM_DI_Lab/Data_Labeling/Data/processed/trainA"
-images = load_comparison_watermarks(path)
-image = images[5]
-image = preprocess_watermark(image)
-
-sys.exit()
