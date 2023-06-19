0a4525b8c3c2b7c927c4cfb94f4277f94f9cc805
EvgheniiBeriozchin <evghenii@r109017.ga1.swh.mhn.de>
2023-06-19 22:49:07 +0200

    Consider geographical source when looking for images

--- utils.py
+++ utils.py
@@ -2,13 +2,13 @@ import os
 
 from typing import List
 from annotation import Annotation
-from constants import RAW_IMAGE_PATH
+from constants import RAW_IMAGE_PATH, GEOGRAPHICAL_SOURCES_PATH
 
 def check_folders_exist(annotations: List[Annotation]):
-    folders = list(set([annotation.path.folder_name for annotation in annotations]))
+    folders = list(set([(annotation.path.geographical_source, annotation.path.folder_name) for annotation in annotations]))
     missing_folders = []
-    for folder in folders:
-        if not os.path.isdir(os.path.join(RAW_IMAGE_PATH, folder)):
+    for geographical_source, folder in folders:
+        if not os.path.isdir(os.path.join(RAW_IMAGE_PATH, GEOGRAPHICAL_SOURCES_PATH[geographical_source], folder)):
             missing_folders.append(folder)
 
     if len(missing_folders) > 0:
