32bd1442d3b941cb6d128ed37e8f84462ecdd61e
EvgheniiBeriozchin <evghenii@r109017.ga1.swh.mhn.de>
2023-07-05 15:39:43 +0200

    Change comment for debugging

--- data/db_insert_annotations.py
+++ data/db_insert_annotations.py
@@ -1,4 +1,5 @@
 import psycopg2
+import os
 from typing import List
 from connect_postgres import get_postgresql_connection
 from annotation import Flag
@@ -7,17 +8,17 @@ from load_annotations import parse_raw_annotations, load_raw_annotations
 
 def insert_annotations_to_db(cursor, annotations):
     for annotation in annotations:
-        print(annotation)
+        print("path: {}".format(os.path.join(annotation.path.folder_name, annotation.path.file_name)))
         # Insert into raw_image table
         cursor.execute(
             """
-            INSERT INTO raw_image (id, path, annotation_id, geographical_region, test_in_image, text_on_watermark, incomplete, multiple_bounding_boxes, no_bounding_boxes, lines_in_bounding_boxes, ruler_in_bounding_boxes, other_markings_in_bounding_boxes)
+            INSERT INTO raw_image (id, path, annotation_id, geographical_region, text_in_image, text_on_watermark, incomplete, multiple_bounding_boxes, no_bounding_boxes, lines_in_bounding_boxes, ruler_in_bounding_boxes, other_markings_in_bounding_boxes)
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
             """,
             (annotation.id,
-                annotation.path.folder_name + "/" + annotation.path.file_name,
-                annotation.source_unique_id,
-                annotation.path.geographical_source.value,
+            os.path.join(annotation.path.folder_name, annotation.path.file_name),
+            annotation.source_unique_id,
+            annotation.path.geographical_source.value,
             any(flag == Flag.TII for flag in annotation.flags),
             any(flag == Flag.TOW for flag in annotation.flags),
             any(flag == Flag.IW for flag in annotation.flags),
