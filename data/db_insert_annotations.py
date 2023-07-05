1c00a8a2e9c2a5cfe64fc3d2f3841736e8f8c1b2
EvgheniiBeriozchin <evghenii@r109017.ga1.swh.mhn.de>
2023-07-05 15:51:12 +0200

    Ignore erroneous inserts

--- data/db_insert_annotations.py
+++ data/db_insert_annotations.py
@@ -10,24 +10,27 @@ def insert_annotations_to_db(cursor, annotations):
     for annotation in annotations:
         print("path: {}".format(os.path.join(annotation.path.folder_name, annotation.path.file_name)))
         # Insert into raw_image table
-        cursor.execute(
-            """
-            INSERT INTO raw_image (id, path, annotation_id, geographical_region, text_in_image, text_on_watermark, incomplete, multiple_bounding_boxes, no_bounding_boxes, lines_in_bounding_boxes, ruler_in_bounding_boxes, other_markings_in_bounding_boxes)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-            """,
-            (annotation.id,
-            os.path.join(annotation.path.folder_name, annotation.path.file_name),
-            annotation.source_unique_id,
-            annotation.path.geographical_source.value,
-            any(flag == Flag.TII for flag in annotation.flags),
-            any(flag == Flag.TOW for flag in annotation.flags),
-            any(flag == Flag.IW for flag in annotation.flags),
-            any(flag == Flag.MBB for flag in annotation.flags),
-            any(flag == Flag.NBB for flag in annotation.flags),
-            any(flag == Flag.SLBB for flag in annotation.flags),
-            any(flag == Flag.RBB for flag in annotation.flags),
-            any(flag == Flag.OMBB for flag in annotation.flags))
-        )
+        try:
+            cursor.execute(
+                """
+                INSERT INTO raw_image (id, path, annotation_id, geographical_region, text_in_image, text_on_watermark, incomplete, multiple_bounding_boxes, no_bounding_boxes, lines_in_bounding_boxes, ruler_in_bounding_boxes, other_markings_in_bounding_boxes)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                """,
+                (annotation.id,
+                os.path.join(annotation.path.folder_name, annotation.path.file_name),
+                annotation.source_unique_id,
+                annotation.path.geographical_source.value,
+                any(flag == Flag.TII for flag in annotation.flags),
+                any(flag == Flag.TOW for flag in annotation.flags),
+                any(flag == Flag.IW for flag in annotation.flags),
+                any(flag == Flag.MBB for flag in annotation.flags),
+                any(flag == Flag.NBB for flag in annotation.flags),
+                any(flag == Flag.SLBB for flag in annotation.flags),
+                any(flag == Flag.RBB for flag in annotation.flags),
+                any(flag == Flag.OMBB for flag in annotation.flags))
+            )
+        except:
+            return
 
 if __name__ == 'main':
     raw_annotations = load_raw_annotations()
