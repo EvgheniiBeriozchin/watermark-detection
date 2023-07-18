b70ecf7f6825a40114abb5390c2c9a56594a31c8
EvgheniiBeriozchin <evghenii@r109017.ga1.swh.mhn.de>
2023-07-19 01:31:48 +0200

    Rename preprocessing folder and add to main structure

--- preprocess_drawings.py
+++ preprocess_drawings.py
@@ -1,12 +1,15 @@
 import cv2
 from numpy import ndarray
+from preprocessing_drawings.PreProcessingDrawings import pre_processing_drawing
 
 def make_black_and_white(image: ndarray):
     result = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 21, 10)
 
     return result
 
-def preprocess_drawing(image: ndarray):
-    image = make_black_and_white(image)
-    
+def preprocess_drawing(image: ndarray, use_classical_preprocessing=False):
+    # image = make_black_and_white(image)
+    if use_classical_preprocessing:
+        image = pre_processing_drawing(image)
+
     return image
